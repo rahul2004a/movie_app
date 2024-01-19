2ea3a325e90cea48dee027c3df7b542736cc9746
rahul2004a <kumar.rahul.2017354@gmail.com>
2024-01-19 16:13:53 +0530

    navbar added

---
+++ src/app/components/Header.jsx
@@ -0,0 +1,19 @@
+import styles from '@/app/styles/navbar.module.css';
+import Image from 'next/image';
+import Link from 'next/link';
+import Nav from "@/app/components/Nav";
+
+const Header = () => {
+    return (
+        <header className={styles.main_header}>
+            <div className={styles.navbar_brand}>
+                <Link href='/'>
+                    <Image src="/logo.png" height={40} width={150} />
+                </Link>
+            </div>
+            <Nav />
+        </header>
+    )
+}
+
+export default Header
